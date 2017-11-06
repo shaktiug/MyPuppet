0f764bfeb205e10bf9bd98d5e4005d1afa51440c
root <root@puppetmaster-01-shakti.com>
2017-11-06 17:47:23 +0530

    added syslog module with hiera config

---
+++ puppet/modules/syslog/manifests/init.pp
@@ -0,0 +1,12 @@
+class syslog {
+$syslogpkg = hiera('syslogpkg','rsyslog')
+package {"$syslogpkg":
+ensure => 'installed',
+}
+service {"$syslogpkg":
+ensure => true,
+hasstatus => true,
+hasrestart => true,
+enable => true,
+}
+}
